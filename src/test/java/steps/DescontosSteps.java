ac0fb6cbe24aa0e821dbdaa6b9e928e2e95f63fb
david.rmelo <david.13rodrigues@hotmail.com>
2023-02-06 19:41:04 -0300

    v0.3

--- src/test/java/steps/DescontosSteps.java
+++ src/test/java/steps/DescontosSteps.java
@@ -3,6 +3,7 @@ package steps;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -15,14 +16,16 @@ public class DescontosSteps {
     @Dado("^que estou no site da qazando$")
     public void que_estou_no_site_da_qazando()   {
         driver.get("https://www.qazando.com.br");
+       // Assert.assertEquals("Não acessou a aplicação",true,driver.findElement(By.id("btn-ver-cursos")).isDisplayed() );
     }
 
     @Quando("^eu preencho meu email$")
     public void eu_preencho_meu_email() throws InterruptedException{
         JavascriptExecutor jse = (JavascriptExecutor)driver;
-        jse.executeScript("window.scrollBy(0,10000)");
-        Thread.sleep(2000);
-        driver.findElement(By.id("email")).sendKeys("david.15rodrigues@gmail.com");
+        jse.executeScript("window.scrollTo(0,10000)");
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("email")).sendKeys("david.13rodrigues@hotmail.com");
     }
 
     @Quando("^clico em ganhar cupom$")
@@ -33,8 +36,10 @@ public class DescontosSteps {
     @Entao("^eu vejo o codigo de desconto$")
     public void eu_vejo_o_codigo_de_desconto()  {
         String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
-        System.out.println(texto_cupom);
+      //  Assert.assertEquals("O cupom está correto!","QAZANDO15OFF",texto_cupom);
         driver.quit();
     }
 
 }
+
+
